7e63263329455644dd11b89849b6736932fbceac
akshay_gaikwad <akshay240695@gmail.com>
2023-05-02 13:43:08 +0530

    Working on shop page

--- shop.js
+++ shop.js
@@ -3,3 +3,17 @@ const applyFilterBtn = document.getElementById("apply-filter-btn");
 applyFilterBtn.addEventListener("click", function() {
   console.log("Apply Filter button clicked");
 })
+
+
+const searchInput = document.getElementById("search-box");
+
+searchInput.addEventListener("keypress", function(e) {
+  if(e.key === "Enter"){
+    console.log("Searching for the desired products....");
+  }
+})
+
+
+const categoryBtns = document.getElementsByClassName("category-filter-btns");
+
+console.log(categoryBtns);
